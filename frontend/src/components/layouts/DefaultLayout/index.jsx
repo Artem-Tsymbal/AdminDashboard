0e4fd2a914059aad01b1afa52101574604260842
Artem <artem@MacBook-Pro-Artem.local>
2021-06-30 10:09:41 +0300

    F-Feature/add default layout

---
+++ frontend/src/components/layouts/DefaultLayout/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useHistory, Link } from 'react-router-dom';
+import './index.scss';
+
+export const DefaultLayout = ({ children }) => {
+  const history = useHistory();
+
+  const onSignOut = () => {
+    history.push('/login');
+  };
+
+  return (
+    <div className="defaultLayout">
+      <header className="defaultLayout__header">
+        <nav className="defaultLayout-nav">
+          <div className="defaultLayout-nav__item">
+            <Link to="/home">Home</Link>
+          </div>
+
+          <div className="defaultLayout-nav__item">
+            <Link to="/admin-panel">Admin Panel</Link>
+          </div>
+
+          <div className="defaultLayout-nav__item">
+            <button type="button" onClick={onSignOut}>
+              Sign Out
+            </button>
+          </div>
+        </nav>
+      </header>
+
+      <main>{children}</main>
+    </div>
+  );
+};
